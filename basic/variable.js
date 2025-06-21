c8c4a8d442413ab42403bda75ea6b4ccd13c68ef
codeWithSonu-fullstac <sonukr75412@gmail.com>
2025-06-21 18:01:24 +0530

    partice done

---
+++ basic/variable.js
@@ -0,0 +1,25 @@
+const accountId=12345
+let email="sk@1234"
+var accountPassward="12345"
+accountCity="japiur";
+let accountState;
+ 
+//accountId=2
+     email="sk@12345"
+     accountPassward="05221"
+     accountCity="Bengular"
+
+    //console.log(accountId);
+    //console.table([accountId,email,accountPassward,accountCity,accountState]);
+    
+    const Student={
+     fullname :"Sonu kumar",
+     age :25,
+     isPass:true,
+     cgpa:9.4
+    };
+    
+    Student["age"]=Student["age"]+1;
+    console.log(Student["age"]);
+    console.log(Student.isPass);
+    
